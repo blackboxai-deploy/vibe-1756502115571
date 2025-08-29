5180b4edc7e9cd23ced5f011209a965fc3d3ce36
blackboxai-deploy <deploy@blackboxai.dev>
2025-08-30 02:45:50 +0530

    Add next.config.ts

---
+++ next.config.ts
@@ -0,0 +1,27 @@
+import type { NextConfig } from 'next'
+
+const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+      },
+      {
+        protocol: 'https',
+        hostname: '**.amazonaws.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'storage.googleapis.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.pexels.com',
+        pathname: '/photos/**',
+      },
+    ],
+  },
+}
+
+export default nextConfig
